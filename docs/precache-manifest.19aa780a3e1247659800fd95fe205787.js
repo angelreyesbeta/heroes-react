39d1cb22722d028c4de83024f9da66ad345ca7d5
Angel Reyes <angelreyesbeta@hotmail.com>
2021-02-23 16:44:36 -0500

    Build de la app

---
+++ docs/precache-manifest.19aa780a3e1247659800fd95fe205787.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae83b99a68d2a6f1241b2de82f2a825",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f5be81dcd9a45efc700",
+    "url": "/static/js/2.006c22f9.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.006c22f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75d9371c221aca6e2e3c",
+    "url": "/static/js/main.5d5dd47d.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
